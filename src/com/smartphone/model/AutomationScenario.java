e9b06584db7265b7c2423cc7afaef1e00d5678d2
Saad-eddine eloddy <eloddysaadeddine@gmail.com>
2026-09-06 20:57:51 +0100

    feature : add getter and setter part 2

--- src/com/smartphone/model/AutomationScenario.java
+++ src/com/smartphone/model/AutomationScenario.java
@@ -14,8 +14,62 @@ public class AutomationScenario {
     private List<Action> actions;
     private boolean enabled;
 
+    public AutomationScenario(String id, String name, String description, trigger trigger, List<Action> actions, boolean enabled) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.trigger = trigger;
+        this.actions = actions;
+        this.enabled = enabled;
+    }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public trigger getTrigger() {
+        return trigger;
+    }
+
+    public void setTrigger(trigger trigger) {
+        this.trigger = trigger;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 //    Methods of AutomationScenario
 //
 //    Yes, it should have methods.
